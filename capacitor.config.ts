9d83d2dde9353c3a77545b1cd8ca56e2a412404e
Gabriel Gaerlan <joshuagaerlan08@gmail.com>
2025-06-09 17:53:40 -0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.c69a83d4c7054569be4d26161d5fbacc',
+  appName: 'Shad Lot Calculator',
+  webDir: 'dist'
+  // server: {
+  //   url: 'https://c69a83d4-c705-4569-be4d-26161d5fbacc.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true
+  // }
+};
+
+export default config;
